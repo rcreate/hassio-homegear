07eac57338c273a211aaf239af0c081a07719050
Ricardo Schmidt <ricardo.schmidt@diemehrwertmacher.de>
2022-04-02 12:41:22 +0200

    change distribution to debian and add gateway + config for cc1101 SPI card

--- homegear/run.sh
+++ homegear/run.sh
@@ -2,6 +2,7 @@
 _term() {
     kill $(cat /var/run/homegear/homegear-management.pid)
     kill $(cat /var/run/homegear/homegear-webssh.pid)
+    kill $(cat /var/run/homegear/homegear-gateway.pid)
     kill $(cat /var/run/homegear/homegear.pid)
     /etc/homegear/homegear-stop.sh
     exit $?
@@ -11,6 +12,7 @@ trap _term SIGTERM
 
 mkdir -p /config/homegear/config /config/homegear/data
 mv -n /main.conf /config/homegear/config/main.conf
+mv -n /gateway.conf /config/homegear/config/gateway.conf
 cp -nR /etc/homegear/* /config/homegear/config
 cp -R /config/homegear/config/* /etc/homegear/
 
@@ -20,6 +22,8 @@ chown -R homegear:homegear /var/log/homegear
 
 mkdir -p /var/run/homegear
 chown homegear:homegear /var/run/homegear
+systemctl enable homegear-gateway
+systemctl restart homegear-gateway
 /etc/homegear/homegear-start.sh
 homegear -u homegear -g homegear -p /var/run/homegear/homegear.pid &
 homegear-management -p /var/run/homegear/homegear-management.pid &
