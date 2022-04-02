c055b6c7fcc857e781cf4687e9652a3a55c4606a
Ricardo Schmidt <ricardo.schmidt@diemehrwertmacher.de>
2022-04-02 21:20:20 +0200

    next try

--- homegear/run.sh
+++ homegear/run.sh
@@ -2,6 +2,7 @@
 _term() {
     kill $(cat /var/run/homegear/homegear-management.pid)
     kill $(cat /var/run/homegear/homegear-webssh.pid)
+    kill $(cat /var/run/homegear/homegear-gateway.pid)
     kill $(cat /var/run/homegear/homegear.pid)
     /etc/homegear/homegear-stop.sh
     exit $?
@@ -12,16 +13,19 @@ trap _term SIGTERM
 mkdir -p /config/homegear/config /config/homegear/config/families
 mv -n /main.conf /config/homegear/config/main.conf
 mv -n /homematicbidcos.conf /config/homegear/config/families/homematicbidcos.conf
+mv -n /gateway.conf /config/homegear/config/gateway.conf
 cp -nR /etc/homegear/* /config/homegear/config
 cp -R /config/homegear/config/* /etc/homegear/
 
-mkdir -p /var/log/homegear
+mkdir -p /var/log/homegear /var/log/homegear-gateway
 touch /var/log/homegear/homegear.log
+touch /var/log/homegear-gateway/homegear-gateway.log
 chown -R homegear:homegear /var/log/homegear
+chown -R homegear:homegear /var/log/homegear-gateway
 
 mkdir -p /var/run/homegear
 chown homegear:homegear /var/run/homegear
-/etc/homegear/homegear-start.sh
+homegear-gateway -p /var/run/homegear-gateway/homegear-gateway.pid &
 homegear -u homegear -g homegear -p /var/run/homegear/homegear.pid &
 homegear-management -p /var/run/homegear/homegear-management.pid &
 tail -f /var/log/homegear/homegear.log &
